f28b05cacb3706116688b38b401061d494b18904
Bernardo-PC <bernardo.vs245@gmail.com>
2025-11-15 14:25:47 -0300

    feat: Create serializer for user model

---
+++ klass_backend/apps/accounts/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = [
+      'id',
+      'username',
+      'email',
+      'registration',
+      'role',
+    ]
+    read_only_fields = [
+      'role',
+    ]
+
+  def validate_registration(self, value):
+    if not value.isdigit():
+      raise serializers.ValidationError("A matrícula deve conter apenas números.")
+
+    if len(value) == 7:
+      return value
+
+    if len(value) == 5:
+      return value
+
+    raise serializers.ValidationError(
+      "Matrícula inválida. Alunos usam 7 dígitos. Professores/Admin usam 5 dígitos."
+    )

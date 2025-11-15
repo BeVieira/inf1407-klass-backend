91a2f5bb01ae03c55b12249b8086f42f10087317
Bernardo-PC <bernardo.vs245@gmail.com>
2025-11-15 14:26:17 -0300

    feat: Create api route

--- klass_backend/apps/accounts/views.py
+++ klass_backend/apps/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+  def get_permissions(self):
+    if self.action in ['create']:
+      return [AllowAny()]  # qualquer um pode criar conta
+    return [IsAuthenticated()]  # listar / atualizar / deletar precisa estar logado

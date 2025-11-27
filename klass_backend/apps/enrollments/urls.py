654a10ed0bfc3a66da0117280ad8360285e898f3
Bernardo-PC <bernardo.vs245@gmail.com>
2025-11-27 12:07:24 -0300

    feat: Create urls

---
+++ klass_backend/apps/enrollments/urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import EnrollmentViewSet
+
+router = DefaultRouter()
+router.register(r"enrollments", EnrollmentViewSet, basename="enrollment")
+
+urlpatterns = [path("", include(router.urls))]

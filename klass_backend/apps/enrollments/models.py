5a190eed016547604a0c67d59f527b7f5c70f48d
Bernardo-PC <bernardo.vs245@gmail.com>
2025-11-27 12:06:21 -0300

    feat: Create enrollment models

--- klass_backend/apps/enrollments/models.py
+++ klass_backend/apps/enrollments/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from apps.courses.models import Section
+
+class Enrollment(models.Model):
+  student = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE,
+    related_name="enrollments",
+  )
+  section = models.ForeignKey(Section, on_delete=models.CASCADE, related_name="enrollments")
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    unique_together = ("student", "section")
+
+  def __str__(self):
+    return f"{self.student} -> {self.section}"
